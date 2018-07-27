a26dec590de90d9d42e0ff15596b545c4c181f8d
Dee Race <deerace12@gmail.com>
2018-07-27 21:00:45 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,12 +7,17 @@ function updateDriverWithKeyAndValue(driver, key, value) {
 }
 
 function destructivelyUpdateDriverWithKeyAndValue(driver, key, value) {
-  const newDriver = Object.assign({}, driver)
-  delete newDriver[key]
-  return newDriver;
+  driver[key] = value
+  return driver
 }
 
 function deleteFromDriverByKey(driver, key) {
-  delete driver.key
-  return driver
+  const newDriver = Object.assign({}, driver)
+  delete newDriver[key];
+  return newDriver;
+}
+
+function destructivelyDeleteFromDriverByKey(driver, key) {
+  delete driver[key]
+  return driver;
 }
