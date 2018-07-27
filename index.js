88609b6f95a8197ed0f9f3c3fc4ca4fc6cf04f47
Dee Race <deerace12@gmail.com>
2018-07-27 20:52:52 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,8 +7,6 @@ function updateDriverWithKeyAndValue(driver, key, value) {
 }
 
 function destructivelyUpdateDriverWithKeyAndValue(driver, key, value) {
-  driver: {
-    name: "Same", 
-    address: "12 Broadway"
-  }
+  driver[key] = value;
+  return driver
 }
